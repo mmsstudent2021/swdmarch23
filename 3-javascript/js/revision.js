7eec29a7fe40857fa366e430f34e101e4cb9f6ed
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-07-29 10:58:27 +0630

    js revision

---
+++ 3-javascript/js/revision.js
@@ -0,0 +1,189 @@
+// const name = 'hein htet zan';
+// let age  = 29;
+
+// console.log(`My name is ${ name }. I was born in ${ 2023 - age }`);
+
+
+// const x = ["a","b","c"];
+
+// // x[7] = "d";
+// x [0] = "000"
+
+// console.log(x);
+
+// let y = null;
+// console.log(y);
+
+// const arr = [];
+
+// arr[0] = "a";
+// arr[1] = "b";
+// arr[1] = "bbb";
+// arr[2] = "c";
+
+// console.log(arr);
+
+
+// const obj={
+//     // a : "aaa",
+//     // b : "bbb",
+//     // c : "ccc"
+// };
+
+
+// obj.a = "aaa";
+// obj.b = "bbb";
+// obj.b = "222";
+// obj.c = "ccc"
+
+// console.log(obj);
+
+// let x = 5;
+// let y = 10;
+
+// console.log(x);
+// console.log(y);
+
+// x = 7;
+
+
+// console.log(x);
+
+// let x = 5;
+// let y = 10;
+
+// let z = x;
+
+// console.log(x);
+// console.log(z);
+
+// x = 7;
+
+// console.log(x);
+// console.log(z);
+
+// console.log({} ? true : false);
+// let a = [];
+// let b = [];
+
+// console.log(a == b);
+
+// let x = ["a","b","c"];
+
+// let y = [];
+
+// console.log(x);
+// console.log(y);
+
+// x[0] = "aaa";
+// console.log(x);
+// console.log(y);
+
+
+
+// function triangle(text = 0) {
+
+//     console.log(`${text}`);
+//     console.log(`${text} ${text}`);
+//     console.log(`${text} ${text} ${text}`);
+
+//     return "this is triangle"
+
+// }
+
+// console.log(triangle("*"));
+// console.log(triangle("x"));
+// console.log(triangle("1"));
+// console.log(triangle());
+
+// const run = function(){
+//     return "this is run";
+// }
+
+// // run = 5;
+
+// console.log(run());
+
+// let x = 5;
+
+// {
+//     console.log(x);
+// }
+
+
+// function run(){
+    
+//     console.log(x);
+// }
+
+// run()
+
+// console.log(run());
+
+// function run(){
+//     return "this is run";
+// }
+
+
+// console.log(x);
+// let x = 5;
+// const obj = {
+//     a : "aaa",
+//     b : "bbb",
+//     c : function(){
+//         return "this is c";
+//     },
+//     d(){
+//         return "this is d"
+//     }
+// }
+
+// console.log(obj.a);
+// console.log(obj.c());
+
+
+// if(false){
+//     console.log("this is true");
+// }else{
+//     console.log("this is false");
+// }
+
+// false && console.log("this is true")
+// false ? console.log("this is true") : console.log("this is false");
+
+
+// for(let i =1;i<= 10; i++){
+//     console.log("*");
+//     console.log("* *");
+//     console.log("* * *");
+//     console.log(i,"loop");
+// }
+
+
+let arr = ["a","b","c","d","e","f"];
+
+const obj = {
+    a : "aaa",
+    b : "bbb",
+    c : "ccc"
+}
+
+
+for(x in obj){
+    console.log(x, obj[x]);
+}
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(i,arr[i]);
+// }
+
+// for(x of arr){
+//     console.log(x);
+// }
+
+// for(x in arr){
+//     console.log(x, arr[x]);
+// }
+
+
+
