988e0bc7750a0216ff746632cb77538e6f0669af
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-10-01 09:44:30 +0630

    text slider

--- 6-vite/vite-libraries/src/main.js
+++ 6-vite/vite-libraries/src/main.js
@@ -1,1 +1,28 @@
+import { Carousel } from "bootstrap";
 import "./app.scss";
+// import * as bootstrap from "bootstrap";
+
+
+
+const sliderElement = document.querySelector("#carouselExampleIndicators");
+// const slider = new bootstrap.Carousel(sliderElement);
+const slider = new Carousel(sliderElement,{
+    interval:2000,
+    ride : "carousel",
+    wrap : true
+});
+
+const textSliderElement = document.querySelector("#textSlide");
+const textSlider = new Carousel(textSliderElement);
+
+
+const next = document.querySelector("#next");
+next.addEventListener("click",() => {
+    slider.next() 
+    textSlider.next()
+    // slider.prev() 
+    // slider.to(2) 
+})
+
+console.log(sliderElement);
+console.log(slider);
